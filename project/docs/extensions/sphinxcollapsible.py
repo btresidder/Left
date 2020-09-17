e67badc2950773d3987568bf33b5c3724991d184
btresidder <btresidder@dxc.com>
2020-09-17 11:58:58 +0930

    collapsible extension 21

--- project/docs/extensions/sphinxcollapsible.py
+++ project/docs/extensions/sphinxcollapsible.py
@@ -5,24 +5,16 @@ class b(nodes.Structural, nodes.Element):
     pass
 
 class Collapsible(Directive):
-
     def run(self):
-        #html_node = nodes.raw(text='''<b>hello world</b>''')
-        
-        #html_node = """<b>This Worked</b>"""
-        #self.body.append(html_node)
-        
         html_node = b()
-        
         return [html_node]
 
 def visit_collapsible_html(self, node):
-    #self.body.append(self.starttag(node, 'b'))
     pass
 
 def depart_collapsible_html(self, node):
-    #self.body.append('test</b>')
     code = """<b>hello world</b>"""
+    code += """<details><summary>Dropdown Title</summary><p>here is some text</p></details>"""
     self.body.append(code)
 
 
