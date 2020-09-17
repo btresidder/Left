90be2065906802ecdb514ec9ebc0f51291ae54fd
btresidder <btresidder@dxc.com>
2020-09-17 10:24:33 +0930

    collapsible extension 4

--- project/docs/extensions/sphinxcollapsible.py
+++ project/docs/extensions/sphinxcollapsible.py
@@ -1,16 +1,25 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive
 
+class useless(docutils.nodes.Element):
+    pass
+
 class Collapsible(Directive):
 
     def run(self):
         paragraph_node = nodes.paragraph(text='</p><h1>hello world<h1><p>')
-        html_node = """<b>This Worked</b>"""
-        self.body.append(html_node)
+        #html_node = """<b>This Worked</b>"""
+        #self.body.append(html_node)
         return [paragraph_node]
+
+def visit_collapsible_html(self, node):
+    self.body.append(self.starttag(node, 'b'))
+def depart_collapsible_html(self, node):
+    self.body.append('test</b>')
 
 def setup(app):
     app.add_directive("collapsible", Collapsible)
+    app.add_node(useless, html=(visit_collapsible_html, depart_collapsible_html))
 
     return {
         'version': '0.1',
