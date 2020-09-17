361498e831319ed3bd15481cb7dec92222def528
btresidder <btresidder@dxc.com>
2020-09-17 13:15:09 +0930

    collapsible extension 23

--- project/docs/extensions/sphinxcollapsible.py
+++ project/docs/extensions/sphinxcollapsible.py
@@ -8,8 +8,13 @@ class b(nodes.Structural, nodes.Element):
 
 # The main class
 class Collapsible(Directive):
+    required_arguments = 0
+    optional_arguments = 0
+    final_argument_whitespace = True
     # Directives.unchanged means u get raw value from the rst
-    option_spec = {'title': directives.unchanged}
+    option_spec = {'title': directives.unchanged,}
+    has_content = True
+    add_index = True
     
     def run(self):
         # Needed to get access to options
