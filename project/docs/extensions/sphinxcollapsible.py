d93b17bf1840cce9066f26247e5530f4b0f15286
btresidder <btresidder@dxc.com>
2020-09-17 15:17:12 +0930

    collapsible extension 49

--- project/docs/extensions/sphinxcollapsible.py
+++ project/docs/extensions/sphinxcollapsible.py
@@ -2,25 +2,27 @@ from docutils import nodes
 from docutils.parsers.rst import Directive
 from docutils.parsers.rst import directives
 
-# Needed to run the visit and depart methods
-# No idea why this class has to exist but its how sphinx works
+# Class represents the node used to start the <details> tag
 class b(nodes.Structural, nodes.Element):
     pass
 
+# Class represents the node used to end the <details> tag
 class c(nodes.Structural, nodes.Element):
     pass
 
 # The main class
 class Collapsible(Directive):
+
+    # Gets content from the directive
     required_arguments = 0
     optional_arguments = 0
     final_argument_whitespace = True
-    # Directives.unchanged means u get raw value from the rst
     option_spec = {'title': directives.unchanged,}
     has_content = True
     add_index = True
     
     def run(self):
+        
         # Needed to get access to options
         global options
         options = self.options
@@ -28,49 +30,40 @@ class Collapsible(Directive):
         global par
         par = nodes.paragraph()
         self.state.nested_parse(self.content, self.content_offset, par)
-        print(par)
-        # Creates the class to call the other methods
+        
+        # Creates the classes to call the other methods
         html_node = b()
         html_node += par
         html_node += c()
+        
         return [html_node]
 
-##class Open_Collapsible(Directive):
-##    pass
-
-##class Close_Collapsible(Directive):
-##    pass
-
-# Visit and depart methods come as a pair
-# Visit is not actually used in this case
+# Visit and depart methods come as pairs
+# Visit creates the collapsible
+# The <details> tag is left open so rst (including directives) can be inserted
 def visit_collapsible_html(self, node):
-    
-    #options = self.options
+
+    # Collapsible is made with <details> tags
     # The title is represented in <summary> tags
     code = """<details><summary><b>"""
     code += options["title"]
     code += """</b></summary>"""
-    
-    # The content of the collapsible goes after the </summary>
-    code += """<p>here is some text</p>"""
-    
     self.body.append(code)
 
-# Creates the collapsible
 def depart_collapsible_html(self, node):
     pass
 
 def visit_col_html(self, node):
     pass
 
+# Closes the <details> tag after rst has been inserted
 def depart_col_html(self, node):
     code = """</details>"""
     self.body.append(code) 
 
+# Setups up directives and nodes
 def setup(app):
     app.add_directive("collapsible", Collapsible)
-    ##app.add_directive("open_collapsible", Open_Collapsible)
-    ##app.add_directive("close_collapsible", Close_Collapsible)
     app.add_node(b, html=(visit_collapsible_html, depart_collapsible_html))
     app.add_node(c, html=(visit_col_html, depart_col_html))
 
