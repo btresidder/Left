c518e40bd7cf8561343b4c7e6bd25ae7789ce33a
btresidder <btresidder@dxc.com>
2020-09-29 08:50:31 +0930

    builder 22

--- project/docs/extensions/sphinxcollapsible.py
+++ project/docs/extensions/sphinxcollapsible.py
@@ -87,8 +87,8 @@ def build_type(app):
 # Setups up directives and nodes
 def setup(app):
     print("in setup")
-    print("app.buildername is: ")
-    print(app.buildername)
+    print("self.buildername is: ")
+    print(self.buildername)
     app.add_directive("collapsible", Collapsible)
     app.add_node(start, html=(visit_collapsible_html, depart_collapsible_html))
     app.add_node(finish, html=(visit_col_html, depart_col_html))
